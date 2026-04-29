103b510e58eced75230daae2bfdad105f976d0f3
oscar-sinaga <oscarmsinaga@gmail.com>
2026-04-29 13:25:54 +0700

    feat: modify automate preprocessing - slice process between train and split

--- preprocessing/automate_Oscar.py
+++ preprocessing/automate_Oscar.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import joblib
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -45,10 +46,14 @@ def preprocess_data(df, scaler_save_path):
     X = df_clean.drop('Target', axis=1)
     y = df_clean['Target']
 
+    # Train test split dilakukan di modelling untuk menjaga konsistensi dengan pipeline modelling
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
     # 7. SCALING (Standardisasi Tahan Outlier)
     print(" -> Melakukan Scaling dengan RobustScaler...")
     scaler = RobustScaler()
-    X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns)
 
     # Menyimpan Scaler untuk Inference/Monitoring
     os.makedirs(os.path.dirname(scaler_save_path), exist_ok=True)
@@ -56,9 +61,10 @@ def preprocess_data(df, scaler_save_path):
     print(f" -> [AMANKAN] Objek Scaler berhasil disimpan di {scaler_save_path}")
 
     # 8. MENGGABUNGKAN KEMBALI
-    df_final = pd.concat([X_scaled, y.reset_index(drop=True)], axis=1)
+    df_final_train = pd.concat([X_train_scaled, y_train.reset_index(drop=True)], axis=1)
+    df_final_test = pd.concat([X_test_scaled, y_test.reset_index(drop=True)], axis=1)
     print("Preprocessing komprehensif selesai.")
-    return df_final
+    return df_final_train, df_final_test
 
 def save_data(df, output_path):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
@@ -69,14 +75,16 @@ if __name__ == "__main__":
     # Tentukan path secara dinamis
     PROJECT_ROOT = os.path.dirname(BASE_DIR)
     RAW_DATA_PATH = os.path.join(PROJECT_ROOT, "predictive_maintenance_raw", "predictive_maintenance.csv")
-    CLEAN_DATA_PATH = os.path.join(BASE_DIR, "predictive_maintenance_preprocessing", "predictive_maintenance_processed.csv")
+    CLEAN_DATA_PATH_TRAIN = os.path.join(BASE_DIR, "predictive_maintenance_preprocessing", "predictive_maintenance_train_processed.csv")
+    CLEAN_DATA_PATH_TEST = os.path.join(BASE_DIR, "predictive_maintenance_preprocessing", "predictive_maintenance_test_processed.csv")
     SCALER_PATH = os.path.join(BASE_DIR, "predictive_maintenance_preprocessing", "scaler.pkl")
 
     # Eksekusi Pipeline
     try:
         raw_df = load_data(RAW_DATA_PATH)
-        clean_df = preprocess_data(raw_df, SCALER_PATH)
-        save_data(clean_df, CLEAN_DATA_PATH)
+        clean_df_train, clean_df_test = preprocess_data(raw_df, SCALER_PATH)
+        save_data(clean_df_train, CLEAN_DATA_PATH_TRAIN)
+        save_data(clean_df_test, CLEAN_DATA_PATH_TEST)
         print("=== PIPELINE PREPROCESSING BERHASIL DIEKSEKUSI ===")
     except Exception as e:
         print(f"=== TERJADI KESALAHAN ===\n{e}")
